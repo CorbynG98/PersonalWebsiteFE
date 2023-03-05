41ab0a5f75f96a2451e4a9673fc0552c394d35a8
Corbyn Greenwood <corbyn.greenwood.98@gmail.com>
2023-03-05 11:42:21 -0500

    More modifications to try and fix deploy issues

--- scripts/gh-pages-deploy.cjs
+++ scripts/gh-pages-deploy.cjs
@@ -4,7 +4,8 @@ import { execa } from 'execa';
 const fs = require('fs');
 (async () => {
   try {
-    await execa('git', ['checkout', '--orphan', 'gh-pages']);
+    const branchName = "gh-pages-test";
+    await execa('git', ['checkout', '--orphan', branchName]);
     // eslint-disable-next-line no-console
     console.log('Building started...');
     await execa('npm', ['run', 'build']);
@@ -17,12 +18,12 @@ const fs = require('fs');
       `./${folderName}/404.html`,
     ]); // Copy the index page and rename 404. Take over github 404 when hit
     await execa('git', ['--work-tree', folderName, 'add', '--all']);
-    await execa('git', ['--work-tree', folderName, 'commit', '-m', 'gh-pages']);
-    console.log('Pushing to gh-pages...');
-    await execa('git', ['push', 'origin', 'HEAD:gh-pages', '--force']);
+    await execa('git', ['--work-tree', folderName, 'commit', '-m', branchName]);
+    console.log('Pushing to ' + branchName + '...');
+    await execa('git', ['push', 'origin', `HEAD:${branchName}`, '--force']);
     await execa('rm', ['-rf', folderName]);
     await execa('git', ['checkout', '-f', 'main']);
-    await execa('git', ['branch', '-D', 'gh-pages']);
+    await execa('git', ['branch', '-D', branchName]);
     console.log('Successfully deployed, check your settings');
   } catch (e) {
     // eslint-disable-next-line no-console
